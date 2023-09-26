ac0d8fc757c74ac064c8244718b08174c872ea24
Emy Ifeora <emyboybeats@gmail.com>
2023-09-26 20:05:13 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# AWS Lambda Serverless Strapi 4 Guide
+
+Check [this blog post](https://blog.vikfand.com/posts/serverless-strapi-4-guide/) for how to deploy Strapi 4 on Lambda with the Serverless framework.
+
+Remember to create a `.env` file if you clone this project. It is recommended to create your Strapi instance with `yarn create strapi-app serverless-strapi-4-guide --quickstart` as you will get the latest version with a recommeded configuration. See the [Strapi docs](https://docs.strapi.io/developer-docs/latest/getting-started/quick-start.html) for more info.
---
+++ config/database.js
@@ -0,0 +1,24 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", ""),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", ""),
+      user: env("DATABASE_USERNAME", ""),
+      password: env("DATABASE_PASSWORD", ""),
+      schema: "public",
+      //todo: add this for production
+      // ssl: {
+      //   rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+      // },
+    },
+    debug: false,
+    // The connection pool can be adjusted, but these minimal limits seem to work well.
+    pool: {
+      min: 0,
+      max: 1,
+      idleTimeoutMillis: 500,
+    },
+  },
+});
